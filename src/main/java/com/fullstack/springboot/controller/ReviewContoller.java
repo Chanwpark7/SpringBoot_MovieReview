6489206590e41382b6027ee82ef0d92bbfe42b27
TJ <TJ@DESKTOP-8FSFFJK>
2024-12-02 12:36:01 +0900

    24.12.02

--- src/main/java/com/fullstack/springboot/controller/ReviewContoller.java
+++ src/main/java/com/fullstack/springboot/controller/ReviewContoller.java
@@ -3,6 +3,7 @@ package com.fullstack.springboot.controller;
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,6 +16,8 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PutMapping;
+
 
 
 
@@ -39,4 +42,19 @@ public class ReviewContoller {
 		return ResponseEntity.ok(reviewNum);
 	}
 	
+	@PutMapping("/{mno}/{reviewNum}")
+	public ResponseEntity<Long> modifyReview(@RequestBody MovieReviewDTO movieReviewDTO) {
+			log.error("수정 요청 번호 "+movieReviewDTO.getMno()+" : "+movieReviewDTO.getReviewnum());
+			
+			movieReviewService.modify(movieReviewDTO);
+		return ResponseEntity.ok(movieReviewDTO.getReviewnum());
+	}
+	
+	@DeleteMapping("/delete/{reviewnum}")
+	public ResponseEntity<Long> deleteReview(@PathVariable("reviewnum") Long reviewnum) {
+		log.error("삭제 요청 번호 "+reviewnum);
+		
+		movieReviewService.remove(reviewnum);
+	return ResponseEntity.ok(reviewnum);
+}
 }
